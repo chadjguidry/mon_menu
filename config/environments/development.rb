6456f31453bbd81f1a641aba3ee8d8175121c98c
Chad Guidry <chadjguidry@gmail.com>
2014-04-07 17:03:05 -0500

    Fix email settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ MonMenu::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -30,5 +30,13 @@ MonMenu::Application.configure do
   # Email settings
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.smtp_settings = {
+      :address              => ENV["SMTP_ADDRESS"],
+      :port                 => ENV["SMTP_PORT"],
+      :domain               => ENV["SMTP_DOMAIN"],
+      :user_name            => ENV["SMTP_USERNAME"],
+      :password             => ENV["SMTP_PASSWORD"],
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+  }
 end
